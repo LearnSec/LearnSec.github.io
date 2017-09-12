eb860fda851fd8ccc87b43f651ee02d4e6a6ce53
root <root@localhost.localdomain>
2017-09-11 23:14:27 -0400

    initial commit2

---
+++ dbconnect.php
@@ -0,0 +1,15 @@
+<?php
+
+$db_host = "108.18.193.234";
+$db_name = "db";
+$db_user = "admin";
+$db_pass = "password";
+
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
